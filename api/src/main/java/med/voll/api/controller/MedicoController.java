afa837c3704e24e6e44906efadfd0a1a40a964d3
luizhakan <luizhakan2@protonmail.com>
2023-08-24 21:42:55 -0300

    Requisiçoes post
    Nesta seçao eu aprendi a fazer requisiçao post no spring boot. aprendi sobre a notaçao @RequestBody, pratiquei o uso do record para utilizar o padrao DTO. enviei dados via POST no formato json.

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,9 @@
 package med.voll.api.controller;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +16,15 @@ import java.io.IOException;
 @RequestMapping("medicos")
 public class MedicoController {
     @PostMapping
-    public void cadastrar(@RequestBody String json) throws IOException {
-        FileWriter escrita = new FileWriter("medico.json");
-        escrita.write(json);
-        escrita.close();
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) throws IOException {
+        System.out.printf("NOME: %s\n", dados.nome());
+        System.out.printf("EMAIL: %s\n", dados.email());
+        System.out.printf("CRM: %s\n", dados.crm());
+        System.out.printf("ENDEREÇO: %s, %s, %s, %s - %s - %s",
+                dados.endereco().logradouro(), dados.endereco().numero(),
+                dados.endereco().bairro(),
+                dados.endereco().cidade(), dados.endereco().uf(),
+                dados.endereco());
+
     }
 }
---
+++ api/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,9 @@
+package med.voll.api.endereco;
+
+public record DadosEndereco(String logradouro,
+                            String bairro,
+                            String cidade,
+                            String uf,
+                            String complemento,
+                            String numero) {
+}
