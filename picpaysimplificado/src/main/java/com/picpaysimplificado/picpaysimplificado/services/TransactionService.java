7555ee119c4cf0d5a7383436d06a4c9bbc98df20
luizhakan <luizhakan2@protonmail.com>
2023-09-09 16:19:06 -0300

    Pause no projeto
    Nesta pausa, eu vi que meus conhecimentos em spring ainda são limitados, fazendo com que eu esteja apenas copiando código, e não aprendendo. por isso, eu retornarei futuramente neste vídeo.
    https://youtu.be/QXunBiLq2SM?si=VjeUTUmPsSQ6sCIi tempo: 42:27

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/picpaysimplificado/services/TransactionService.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.picpaysimplificado.services;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.picpaysimplificado.dtos.TransactionDTO;
+import com.picpaysimplificado.picpaysimplificado.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    public void createTransaction(TransactionDTO transaction) throws Exception {
+        User sender = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiverId());
+
+        userService.validateTransaction(sender, transaction.value());
+
+        if ()
+    }
+
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+
+    }
+}
